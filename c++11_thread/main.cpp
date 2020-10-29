9c040423db9e33473fe51dd9a26e02374914ef7a
chow Choi <chowchoi@chowdeMacBook-Pro.local>
2020-10-30 00:08:42 +0800

    Initial commit

---
+++ c++11_thread/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  c++11_thread
+//
+//  Created by chow Choi on 2020/10/29.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
